55b292f7dc9d8ab9001ed3a7f49ac0d72063fb37
limye2 <144992260+limye2@users.noreply.github.com>
2023-09-16 00:43:33 +0900

    Initial Commit

---
+++ 02/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  02
+//
+//  Created by MacBook Air on 2023/09/16.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, Sookmyung Electronics!\n");
+    return 0;
+}
